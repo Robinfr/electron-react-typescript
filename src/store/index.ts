14c3a4e5179c223603bce7aafa7afc3e5babfac4
Robin Franken <rfranken.prive@gmail.com>
2017-12-23 15:18:12 +0100

    Prettified the files

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,23 +4,17 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer, RootState } from '../reducers';
 
 const configureStore = (initialState?: RootState): Store<RootState | undefined> => {
-    const middlewares: any[] = [
-
-    ];
-    const enhancer = composeWithDevTools(
-        applyMiddleware(...middlewares)
-    );
-    return createStore<RootState | undefined>(
-        rootReducer,
-        initialState,
-        enhancer
-    );
+    const middlewares: any[] = [];
+    const enhancer = composeWithDevTools(applyMiddleware(...middlewares));
+    return createStore<RootState | undefined>(rootReducer, initialState, enhancer);
 };
 
 const store = configureStore();
 
 if (typeof module.hot !== 'undefined') {
-    module.hot.accept('../reducers', () => store.replaceReducer(require('../reducers').rootReducer));
+    module.hot.accept('../reducers', () =>
+        store.replaceReducer(require('../reducers').rootReducer)
+    );
 }
 
 export default store;

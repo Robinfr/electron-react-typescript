266eac32c6113822046e66f4feef6ab11ff65599
Robin Franken <rfranken.prive@gmail.com>
2017-12-26 15:32:38 +0100

    Add UglifyJS to prod scripts

--- webpack.renderer.dev.config.js
+++ webpack.renderer.dev.config.js
@@ -5,7 +5,6 @@ const merge = require('webpack-merge');
 const baseConfig = require('./webpack.renderer.config');
 
 module.exports = merge.smart(baseConfig, {
-    target: 'electron-renderer',
     entry: [
         'react-hot-loader/patch',
         './src/app.tsx'
